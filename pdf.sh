6a8e496dc516a7759ae36249a5afc8057ee13d5b
Gwannon <monclus.jorge@gmail.com>
2024-04-11 09:29:59 +0200

    Versión en inglés

--- pdf.sh
+++ pdf.sh
@@ -2,4 +2,8 @@
 
 chromium --no-sandbox --headless ---gpu --no-pdf-header-footer --disable-back-forward-cache-for-cache-control-no-store-page --print-to-pdf=./Kardtana.pdf ./kardtana.html
 
-exiftool -overwrite_original -Title="Kardtana - Un juego de duelos de samurais en una sola carta." ./Kardtana.pdf
+exiftool -overwrite_original -Title="Kardtana - El juego de duelos de samuráis rompiendo cartas." ./Kardtana.pdf
+
+chromium --no-sandbox --headless ---gpu --no-pdf-header-footer --disable-back-forward-cache-for-cache-control-no-store-page --print-to-pdf=./Kardtana_en.pdf ./kardtana_en.html
+
+exiftool -overwrite_original -Title="Kardtana - The card-breaking samurai duel game." ./Kardtana_en.pdf
